3b9cde19bb2faf3ae534013143f02f54f963b88c
Dmytro <dmytroivanovichn@gmail.com>
2024-12-02 21:44:46 +0200

    first commit

---
+++ src/js/files/sliders.js
@@ -0,0 +1,135 @@
+/*
+Документація по роботі у шаблоні: 
+Документація слайдера: https://swiperjs.com/
+Сніппет(HTML): swiper
+*/
+
+// Підключаємо слайдер Swiper з node_modules
+// При необхідності підключаємо додаткові модулі слайдера, вказуючи їх у {} через кому
+// Приклад: { Navigation, Autoplay }
+import Swiper from 'swiper';
+import { Navigation, Grid } from 'swiper/modules';
+/*
+Основні модулі слайдера:
+Navigation, Pagination, Autoplay, 
+EffectFade, Lazy, Manipulation
+Детальніше дивись https://swiperjs.com/
+*/
+
+// Стилі Swiper
+// Базові стилі
+import "../../scss/base/swiper.scss";
+// Повний набір стилів з scss/libs/swiper.scss
+import "../../scss/libs/swiper.scss";
+// Повний набір стилів з node_modules
+// import 'swiper/css';
+
+// Ініціалізація слайдерів
+function initSliders() {
+	// Список слайдерів
+
+	if (document.querySelector('.team-swiper')) {
+		// Створюємо слайдер
+		new Swiper('.team-swiper', { 
+			modules: [Navigation, Grid],
+			slidesPerView: 3,
+			grid: {
+			  rows: 2, 
+			  fill: 'row', 
+			},
+			spaceBetween: 30,
+			navigation: {
+				prevEl: '.our-team__prev',
+				nextEl: '.our-team__next',
+			},
+
+			//touchRatio: 0,
+			//simulateTouch: false,
+			//loop: true,
+			//preloadImages: false,
+			//lazy: true,
+
+			/*
+			// Ефекти
+			effect: 'fade',
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false,
+			},
+			*/
+
+			// Скроллбар
+			/*
+			scrollbar: {
+				el: '.swiper-scrollbar',
+				draggable: true,
+			},
+			*/
+
+			// Кнопки "вліво/вправо"
+
+			/*
+			// Брейкпоінти
+			breakpoints: {
+				640: {
+					slidesPerView: 1,
+					spaceBetween: 0,
+					autoHeight: true,
+				},
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20,
+				},
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20,
+				},
+				1268: {
+					slidesPerView: 4,
+					spaceBetween: 30,
+				},
+			},
+			*/
+			// Події
+			on: {
+
+			}
+		});
+	}
+}
+// Скролл на базі слайдера (за класом swiper scroll для оболонки слайдера)
+function initSlidersScroll() {
+	let sliderScrollItems = document.querySelectorAll('.swiper_scroll');
+	if (sliderScrollItems.length > 0) {
+		for (let index = 0; index < sliderScrollItems.length; index++) {
+			const sliderScrollItem = sliderScrollItems[index];
+			const sliderScrollBar = sliderScrollItem.querySelector('.swiper-scrollbar');
+			const sliderScroll = new Swiper(sliderScrollItem, {
+				observer: true,
+				observeParents: true,
+				direction: 'vertical',
+				slidesPerView: 'auto',
+				freeMode: {
+					enabled: true,
+				},
+				scrollbar: {
+					el: sliderScrollBar,
+					draggable: true,
+					snapOnRelease: false
+				},
+				mousewheel: {
+					releaseOnEdges: true,
+				},
+			});
+			sliderScroll.scrollbar.updateSize();
+		}
+	}
+}
+
+window.addEventListener("load", function (e) {
+	// Запуск ініціалізації слайдерів
+	initSliders();
+	console.log('init slider')
+	// Запуск ініціалізації скролла на базі слайдера (за класом swiper_scroll)
+	//initSlidersScroll();
+});
